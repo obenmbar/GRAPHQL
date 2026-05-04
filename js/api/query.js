0639e1ad797990ded809ccadb8928063150c9207
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-05-04 09:07:46 +0100

    debugingfin

--- js/api/query.js
+++ js/api/query.js
@@ -1,12 +1,44 @@
- export const Query = `
-    query {
-      user { id login firstName lastName auditRatio }
-      xp_transactions: transaction(
-        where: { type: { _eq: "xp" }, path: { _nlike: "%/piscine-%" } }
-        order_by: { createdAt: asc }
-      ) { amount path createdAt }
-      skills: transaction(
-        where: { type: { _like: "skill_%" } }
-        order_by: { amount: desc }
-      ) { type amount }
-    }`;
+export const Query = `
+query GetMyProfile {
+  user {
+    id
+    login
+    firstName
+    lastName
+    auditRatio
+    avatarUrl  
+    auditSuccess: audits_aggregate(where: { closureType: { _eq: succeeded } }) {
+      aggregate { count }
+    }
+    auditFail: audits_aggregate(where: { closureType: { _eq: failed } }) {
+      aggregate { count }
+    }
+  }
+  
+ 
+  total_xp: transaction_aggregate(
+    where: { type: { _eq: "xp" }, event: { object: { name: { _eq: "Module" } } } }
+  ) {
+    aggregate { sum { amount } }
+  }
+
+
+  xp_transactions: transaction(
+    where: { type: { _eq: "xp" }, event: { object: { name: { _eq: "Module" } } } }
+    order_by: { createdAt: asc }
+  ) {
+    amount
+    path
+    createdAt
+  }
+
+
+  skills: transaction(
+    where: { type: { _like: "skill_%" } }
+    order_by: { amount: desc }
+  ) {
+    type
+    amount
+  }
+}
+`;
